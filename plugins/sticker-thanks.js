c1e7154ae3481a2be93152e95e0f336e24f9be92
SDyka <109986388+SDhyka@users.noreply.github.com>
2022-09-15 16:48:19 +0700

    Add files via upload

--- plugins/sticker-thanks.js
+++ plugins/sticker-thanks.js
@@ -8,7 +8,7 @@ let stiker = await sticker(null, global.API(`${pickRandom(stikerhuuu)}`), global
     
 }
 
-handler.customPrefix = /^(thx|thank|thanks)$/i
+handler.customPrefix = /^(.thx|.thank|.thanks)$/i
 handler.command = new RegExp
 
 export default handler
